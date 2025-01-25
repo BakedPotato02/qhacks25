47fdcba8745408760c9f545ffd58341343656b40
Jawad Ahmed <97852920+BakedPotato02@users.noreply.github.com>
2025-01-25 05:00:24 -0500

    renaming, intro structure complete

--- jawad/script.js
+++ jawad/script.js
@@ -1,10 +1,3 @@
-function togglePopup() {
-    const popup = document.getElementById('popup');
-    // Toggle the popup visibility (show or hide)
-    if (popup.style.display === 'flex') {
-      popup.style.display = 'none';
-    } else {
-      popup.style.display = 'flex';
-    }
-  }
-  
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('.p1').classList.add('p1');
+});
